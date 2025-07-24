12bf379535917b87b79935b7228b0d3f4c9ac47c
shyamsoni550 <vayashyam550@gmail.com>
2025-07-24 14:04:32 +0530

    weather-api

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cityinput = document.getElementById("city-input");
+    const getWeatherBtn = document.getElementById("get-weather-btn");
+    const weatherinfo = document.getElementById("weather-info");
+    const citynametext = document.getElementById("city-name-text");
+    const tempreturetext = document.getElementById("tempreature-text");
+    const descriptiontext = document.getElementById("description-text");
+    const errormessage = document.getElementById("error-message");
+    const API_KEY = "cec0bd74b41cc115a9d56215f789c118";
+
+    getWeatherBtn.addEventListener("click", async () => {
+        const city = cityinput.value.trim();
+        if (!city) return;
+
+        try {
+            const weatherdata = await fetchWeatherData(city);
+            displayWeatherData(weatherdata);
+        } catch (error) {
+            showerror();
+        }
+    });
+
+    async function fetchWeatherData(city) {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("city not found");
+        }
+        const data = await response.json();
+        return data;
+    }
+
+    function displayWeatherData(data) {
+        const { main, name, weather } = data;
+        citynametext.textContent = name;
+        tempreturetext.textContent = `${main.temp} °C`;
+        descriptiontext.textContent = weather[0].description;
+
+        weatherinfo.classList.remove("hidden");
+        errormessage.classList.add("hidden");
+    }
+
+    function showerror() {
+        weatherinfo.classList.add('hidden');
+        errormessage.classList.remove('hidden');
+    }
+});
